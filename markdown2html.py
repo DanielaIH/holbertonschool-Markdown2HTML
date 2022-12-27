2a146b57a59d31cbc09fd1247907134c91c1e6c1
DanielaIH <4427@holbertonschool.com>
2022-12-26 21:50:57 -0500

    ADD task 1

--- markdown2html.py
+++ markdown2html.py
@@ -6,7 +6,7 @@ First argument is the name of the Markdown file
 Second argument is the output file name
 '''
 
-from sys import argv
+from sys import argv, stderr
 from os import path
 
 
@@ -14,16 +14,16 @@ def markdown2html():
     """main program"""
     import markdown
     if len(argv) < 2:
-        print("Usage: ./markdown2html.py README.md README.html")
+        print("Usage: ./markdown2html.py README.md README.html", file=stderr)
         exit(1)
     if len(argv) >= 2:
         if not path.exists(argv[1]):
-            print("Missing " + argv[1])
+            print("Missing " + argv[1], file=stderr)
             exit(1)
         with open(argv[1], "r") as f:
             markdown = markdown.markdown(f.read())
         with open(argv[2], "w") as f:
-            f.write(markdown)
+            f.write(markdown + "\n")
         exit(0)
 
 
