f64c6732d4b870f71d3450a91b32531125e0cba1
DanielaIH <4427@holbertonschool.com>
2022-12-26 21:15:23 -0500

    ADD task 1

---
+++ markdown2html.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This is a script markdown2html.py"""
+
+
+from sys import argv
+from os import path
+import markdown
+
+if __name__ == "__main__":
+
+    if len(argv) < 2:
+        print("Usage: ./markdown2html.py README.md README.html")
+        exit(1)
+    if len(argv) >= 2:
+        if not path.exists(argv[1]):
+            print("Missing " + argv[1])
+            exit(1)
+        with open(argv[1], "r") as f:
+            markdown = markdown.markdown(f.read())
+        with open(argv[2], "w") as f:
+            f.write(markdown)
+        exit(0)
